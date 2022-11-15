766cfd4859443a5398bd307d30488563b4fdba6c
SolardiaX <solardiax@hotmail.com>
2022-11-15 15:48:37 +0800

    feat: try move position of TalkingHead

--- DarkUI/Modules/Blizzard/TalkingHead.lua
+++ DarkUI/Modules/Blizzard/TalkingHead.lua
@@ -6,26 +6,23 @@ if not C.blizzard.custom_position then return end
 ------------------------------------------------------------------------------------------
 local Load = CreateFrame("Frame")
 Load:SetPoint(unpack(C.blizzard.talking_head_pos))
-Load:RegisterEvent("ADDON_LOADED")
+Load:RegisterEvent("PLAYER_ENTERING_WORLD")
 Load:SetScript("OnEvent", function(_, _, addon)
-    if addon == E.addonName then
-        if not _G.TalkingHeadFrame then
-            _G.TalkingHead_LoadUI()
-        end
-        
-        TalkingHeadFrame.ignoreFramePositionManager = true
-        TalkingHeadFrame:SetParent(Load)
-        TalkingHeadFrame:ClearAllPoints()
-        TalkingHeadFrame:SetPoint("TOP", Load, "TOP")
-
-        --Iterate through all alert subsystems in order to find the one created for TalkingHeadFrame, and then remove it.
-        --We do this to prevent alerts from anchoring to this frame when it is shown.
-        for index, alertFrameSubSystem in ipairs(_G.AlertFrame.alertFrameSubSystems) do
-            if alertFrameSubSystem.anchorFrame and alertFrameSubSystem.anchorFrame == _G.TalkingHeadFrame then
-                tremove(_G.AlertFrame.alertFrameSubSystems, index)
-            end
-        end
+    if not _G.TalkingHeadFrame then
+        _G.TalkingHead_LoadUI()
+    end
     
-        Load:UnregisterEvent("ADDON_LOADED")
+    TalkingHeadFrame.ignoreFramePositionManager = true
+    TalkingHeadFrame.ignoreFrameLayout = true
+    TalkingHeadFrame:SetParent(Load)
+    TalkingHeadFrame:ClearAllPoints()
+    TalkingHeadFrame:SetPoint("TOP", Load, "TOP")
+
+    --Iterate through all alert subsystems in order to find the one created for TalkingHeadFrame, and then remove it.
+    --We do this to prevent alerts from anchoring to this frame when it is shown.
+    for index, alertFrameSubSystem in ipairs(_G.AlertFrame.alertFrameSubSystems) do
+        if alertFrameSubSystem.anchorFrame and alertFrameSubSystem.anchorFrame == _G.TalkingHeadFrame then
+            tremove(_G.AlertFrame.alertFrameSubSystems, index)
+        end
     end
 end)
