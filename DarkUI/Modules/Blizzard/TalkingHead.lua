2fdd62d7a94b75211bc458b89216e64f5daf723b
SolardiaX <solardiax@hotmail.com>
2022-11-15 13:25:02 +0800

    feat: try custom talkinghead position

--- DarkUI/Modules/Blizzard/TalkingHead.lua
+++ DarkUI/Modules/Blizzard/TalkingHead.lua
@@ -8,11 +8,24 @@ local Load = CreateFrame("Frame")
 Load:SetPoint(unpack(C.blizzard.talking_head_pos))
 Load:RegisterEvent("ADDON_LOADED")
 Load:SetScript("OnEvent", function(_, _, addon)
-    if addon == "Blizzard_TalkingHeadUI" or (addon == E.addonName and TalkingHeadFrame) then
+    if addon == E.addonName then
+        if not _G.TalkingHeadFrame then
+            _G.TalkingHead_LoadUI()
+        end
+        
         TalkingHeadFrame.ignoreFramePositionManager = true
         TalkingHeadFrame:SetParent(Load)
         TalkingHeadFrame:ClearAllPoints()
         TalkingHeadFrame:SetPoint("TOP", Load, "TOP")
-	    Load:UnregisterEvent("ADDON_LOADED")
+
+        --Iterate through all alert subsystems in order to find the one created for TalkingHeadFrame, and then remove it.
+        --We do this to prevent alerts from anchoring to this frame when it is shown.
+        for index, alertFrameSubSystem in ipairs(_G.AlertFrame.alertFrameSubSystems) do
+            if alertFrameSubSystem.anchorFrame and alertFrameSubSystem.anchorFrame == _G.TalkingHeadFrame then
+                tremove(_G.AlertFrame.alertFrameSubSystems, index)
+            end
+        end
+    
+        Load:UnregisterEvent("ADDON_LOADED")
     end
 end)
