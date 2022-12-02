60dca5eaa24eb71a73867d9938f6ea66c6dd21e8
SolardiaX <solardiax@hotmail.com>
2022-12-02 14:59:13 +0800

    feat: [Misc] check missing enchants and gems

--- DarkUI/Modules/Misc/SlotItemLevel.lua
+++ DarkUI/Modules/Misc/SlotItemLevel.lua
@@ -6,6 +6,7 @@ if not C.misc.slot_itemlevel then return end
 --	Item level on slot buttons in Character/InspectFrame(iLevel by Sanex)
 ----------------------------------------------------------------------------------------
 
+local minItemLevel = 197 -- For missing enchant and gems checking
 local _G = getfenv(0)
 local equiped = {} -- Table to store equiped items
 
@@ -76,11 +77,31 @@ local function _updateItems(unit, frame)
             if realItemLevel and tonumber(realItemLevel) == 1 then
                 realItemLevel = ""
             end
+
             local color = "|cffFFFF00"
-            if itemLink and (i == 15 or i == 5 or i == 16 or i == 11 or i == 12 or i == itemSlot) and (realItemLevel ~= "" and tonumber(realItemLevel) > 197) then
-                local _, _, enchant = strsplit(":", itemLink)
-                if enchant and enchant == "" then
-                    color = "|cffFF0000"
+            
+            -- Check missing enchants and gems
+            if itemLink and (realItemLevel ~= "" and tonumber(realItemLevel) > minItemLevel) then
+                local _, _, enchant, gem1, gem2, gem3 = strsplit(":", itemLink)
+                if i == 15 or i == 5 or i == 16 or i == 11 or i == 12 or i == itemSlot then
+                    if enchant and enchant == "" then
+                        color = "|cffFF0000"
+                    end
+                end
+
+                local info = GetItemStats(itemLink)
+                local numSocket = info["EMPTY_SOCKET_PRISMATIC"] or 0
+                local numGem = 0
+
+                if gem1 and gem1 ~= "" then
+                    numGem = numGem + 1
+                elseif gem2 and gem2 ~= "" then
+                    numGem = numGem + 1
+                elseif gem3 and gem3 ~= "" then
+                    numGem = numGem + 1
+                end
+                if numGem < numSocket then
+                    color = "|cffFF00CC"
                 end
             end
 
