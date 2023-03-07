15b405a1e0dd448ae7ce04bc3d2367ed062a693d
SolardiaX <solardiax@hotmail.com>
2023-03-07 11:18:32 +0800

    fix: [Blizzard] tooltip issues

--- DarkUI/Modules/Blizzard/Fixes.lua
+++ DarkUI/Modules/Blizzard/Fixes.lua
@@ -3,33 +3,33 @@ local E, C, L = select(2, ...):unpack()
 ----------------------------------------------------------------------------------------
 --	Fix blank tooltip
 ----------------------------------------------------------------------------------------
-local bug = nil
-local FixTooltip = CreateFrame("Frame")
-FixTooltip:RegisterEvent("UPDATE_BONUS_ACTIONBAR")
-FixTooltip:RegisterEvent("ACTIONBAR_PAGE_CHANGED")
-FixTooltip:SetScript("OnEvent", function()
-    if GameTooltip:IsShown() then
-        bug = true
-    end
-end)
+-- local bug = nil
+-- local FixTooltip = CreateFrame("Frame")
+-- FixTooltip:RegisterEvent("UPDATE_BONUS_ACTIONBAR")
+-- FixTooltip:RegisterEvent("ACTIONBAR_PAGE_CHANGED")
+-- FixTooltip:SetScript("OnEvent", function()
+--     if GameTooltip:IsShown() then
+--         bug = true
+--     end
+-- end)
 
-local FixTooltipBags = CreateFrame("Frame")
-FixTooltipBags:RegisterEvent("BAG_UPDATE_DELAYED")
-FixTooltipBags:SetScript("OnEvent", function()
-    if StuffingFrameBags and StuffingFrameBags:IsShown() then
-        if GameTooltip:IsShown() then
-            bug = true
-        end
-    end
-end)
+-- local FixTooltipBags = CreateFrame("Frame")
+-- FixTooltipBags:RegisterEvent("BAG_UPDATE_DELAYED")
+-- FixTooltipBags:SetScript("OnEvent", function()
+--     if StuffingFrameBags and StuffingFrameBags:IsShown() then
+--         if GameTooltip:IsShown() then
+--             bug = true
+--         end
+--     end
+-- end)
 
-GameTooltip:HookScript("OnTooltipCleared", function(self)
-    if self:IsForbidden() then return end
-    if bug and self:NumLines() == 0 then
-        self:Hide()
-        bug = false
-    end
-end)
+-- GameTooltip:HookScript("OnTooltipCleared", function(self)
+--     if self:IsForbidden() then return end
+--     if bug and self:NumLines() == 0 then
+--         self:Hide()
+--         bug = false
+--     end
+-- end)
 
 ----------------------------------------------------------------------------------------
 --	Fix RemoveTalent() taint
@@ -39,12 +39,12 @@ end)
 ----------------------------------------------------------------------------------------
 --	Fix SearchLFGLeave() taint
 ----------------------------------------------------------------------------------------
-local TaintFix = CreateFrame("Frame")
-TaintFix:SetScript("OnUpdate", function()
-    if LFRBrowseFrame.timeToClear then
-        LFRBrowseFrame.timeToClear = nil
-    end
-end)
+-- local TaintFix = CreateFrame("Frame")
+-- TaintFix:SetScript("OnUpdate", function()
+--     if LFRBrowseFrame.timeToClear then
+--         LFRBrowseFrame.timeToClear = nil
+--     end
+-- end)
 
 ----------------------------------------------------------------------------------------
 --	Collect garbage
