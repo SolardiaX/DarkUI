e517809de96d0aba2df2798fecf27d5501163ce2
SolardiaX <solardiax@hotmail.com>
2023-05-09 11:15:35 +0800

    fix: [Quest] auto collapse issue

--- DarkUI/Modules/Quest/AutoCollapse.lua
+++ DarkUI/Modules/Quest/AutoCollapse.lua
@@ -11,10 +11,47 @@ local IsInInstance, InCombatLockdown = IsInInstance, InCombatLockdown
 local ObjectiveTracker_Collapse, ObjectiveTracker_Expand = ObjectiveTracker_Collapse, ObjectiveTracker_Expand
 local ObjectiveTrackerFrame = ObjectiveTrackerFrame
 
+local headers = {
+	SCENARIO_CONTENT_TRACKER_MODULE,
+	BONUS_OBJECTIVE_TRACKER_MODULE,
+	UI_WIDGET_TRACKER_MODULE,
+	CAMPAIGN_QUEST_TRACKER_MODULE,
+	QUEST_TRACKER_MODULE,
+	ACHIEVEMENT_TRACKER_MODULE,
+	WORLD_QUEST_TRACKER_MODULE,
+	PROFESSION_RECIPE_TRACKER_MODULE,
+	MONTHLY_ACTIVITIES_TRACKER_MODULE
+}
+
 module:RegisterEvent("PLAYER_ENTERING_WORLD", function()
-	if IsInInstance() then
-		ObjectiveTracker_Collapse()
-	elseif ObjectiveTrackerFrame.collapsed and not InCombatLockdown() then
-		ObjectiveTracker_Expand()
-	end
+    local inInstance, instanceType = IsInInstance()
+
+    if inInstance then
+        if instanceType == "party" or instanceType == "scenario" then
+            if instanceType == "party" or instanceType == "scenario" then
+                C_Timer.After(0.1, function() -- for some reason it got error after reload in instance
+                    for i = 3, #headers do
+                        local button = headers[i].Header.MinimizeButton
+                        if button and not headers[i].collapsed then
+                            button:Click()
+                        end
+                    end
+                end)
+            else
+                C_Timer.After(0.1, function()
+                    ObjectiveTracker_Collapse()
+                end)
+            end
+        end
+    elseif not InCombatLockdown() then
+        for i = 3, #headers do
+            local button = headers[i].Header.MinimizeButton
+            if button and headers[i].collapsed then
+                button:Click()
+            end
+        end
+        if ObjectiveTrackerFrame.collapsed then
+            ObjectiveTracker_Expand()
+        end
+    end
 end)
