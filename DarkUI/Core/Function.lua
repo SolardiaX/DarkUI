41dbaeeefa66cd71af871063f54ede47944eabfd
SolardiaX <solardiax@hotmail.com>
2024-08-21 11:00:19 +0800

    fix: missing EasyMenu function

--- DarkUI/Core/Function.lua
+++ DarkUI/Core/Function.lua
@@ -186,3 +186,24 @@ function E:SetVariable(group, key, value)
         index = index + 1
     end
 end
+
+----------------------------------------------------------------------------------------
+--    Global EasyMenu function
+----------------------------------------------------------------------------------------
+local function EasyMenu_Initialize(frame, level, menuList)
+	for index = 1, #menuList do
+		local value = menuList[index]
+		if value.text then
+			value.index = index
+			UIDropDownMenu_AddButton(value, level)
+		end
+	end
+end
+
+function EasyMenu(menuList, menuFrame, anchor, x, y, displayMode, autoHideDelay)
+	if displayMode == "MENU"  then
+		menuFrame.displayMode = displayMode
+	end
+	UIDropDownMenu_Initialize(menuFrame, EasyMenu_Initialize, displayMode, nil, menuList)
+	ToggleDropDownMenu(1, nil, menuFrame, anchor, x, y, menuList, nil, autoHideDelay)
+end
