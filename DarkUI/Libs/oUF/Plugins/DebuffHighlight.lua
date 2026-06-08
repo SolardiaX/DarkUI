6dd249aa4f7e7c799bf06cfb2fc87b728d203d4b
SolardiaX <solardiax@hotmail.com>
2026-06-09 07:31:34 +0800

    refactor: [Libs] rewrite DebuffHighlight with 12.0 ColorCurve API
    Replace manual aura iteration with RAID_PLAYER_DISPELLABLE filter and
    C_UnitAuras.GetAuraDispelTypeColor + ColorCurve. Add talent-aware
    dispel detection (Evoker Cauterizing Flame, Paladin Cleanse Toxins,
    Shaman Poison Cleansing Totem). Retain existing display interface
    (DebuffHighlight texture, DebuffHighlightAlpha, Backdrop modes).

--- DarkUI/Libs/oUF/Plugins/DebuffHighlight.lua
+++ DarkUI/Libs/oUF/Plugins/DebuffHighlight.lua
@@ -1,135 +1,215 @@
-----------------------------------------------------------------------------------------
---    Based on oUF_DebuffHighlight(by Ammo)
-----------------------------------------------------------------------------------------
+------------------------------------------------------------------------
+-- DebuffHighlight (based on ShestakUI DispelColor, adapted for DarkUI)
+------------------------------------------------------------------------
 local _, ns = ...
 local oUF = ns.oUF
 
-local class = select(2, UnitClass('player'))
+local class = select(2, UnitClass("player"))
 
 local CanDispel = {
-    DRUID = {Magic = false, Curse = true, Poison = true},
-    EVOKER = {Magic = false, Curse = true, Poison = true, Disease = true},
-    MAGE = {Curse = true},
-    MONK = {Magic = false, Poison = true, Disease = true},
-    PALADIN = {Magic = false, Poison = true, Disease = true},
-    PRIEST = {Magic = false, Disease = true},
-    SHAMAN = {Magic = false, Curse = true}
+    DRUID = { Magic = false, Curse = true, Poison = true },
+    EVOKER = { Magic = false, Curse = true, Poison = true, Disease = true },
+    MAGE = { Curse = true },
+    MONK = { Magic = false, Poison = true, Disease = true },
+    PALADIN = { Magic = false, Poison = true, Disease = true },
+    PRIEST = { Magic = false, Disease = true },
+    SHAMAN = { Magic = false, Curse = true },
 }
 
 local dispellist = CanDispel[class] or {}
+
+local color_magic = CreateColor(0, 0, 0, 0)
+local color_curse = CreateColor(0, 0, 0, 0)
+local color_disease = CreateColor(0, 0, 0, 0)
+local color_poison = CreateColor(0, 0, 0, 0)
+local color_bleed = CreateColor(0, 0, 0, 0)
+
+local dispelColorCurve = C_CurveUtil.CreateColorCurve()
+dispelColorCurve:SetType(Enum.LuaCurveType.Step)
+
 local origColors = {}
 local origBorderColors = {}
 
-local function GetDebuffType(unit, filter)
-    if not UnitCanAssist("player", unit) then return nil end
-    local i = 1
-    while true do
-        local auraData = C_UnitAuras.GetAuraDataByIndex(unit, i, "HARMFUL")
-        if not auraData then break end
-        if not auraData.texture then break end
-        if auraData.dispelName and not filter or (filter and dispellist[auraData.dispelName]) then
-            return auraData.debufftype, auraData.texture
-        end
-        i = i + 1
-    end
-end
-
 local function CheckSpec()
-    local spec = GetSpecialization()
+    local spec = C_SpecializationInfo.GetSpecialization()
 
     if class == "DRUID" then
         if spec == 4 then
+            color_magic = CreateColor(0.2, 0.6, 1)
             dispellist.Magic = true
         else
+            color_magic = CreateColor(0, 0, 0, 0)
             dispellist.Magic = false
         end
+    elseif class == "EVOKER" then
+        if spec == 2 then
+            color_magic = CreateColor(0.2, 0.6, 1)
+            dispellist.Magic = true
+        else
+            color_magic = CreateColor(0, 0, 0, 0)
+            dispellist.Magic = false
+        end
+        if C_SpellBook.IsSpellKnown(374251) then
+            color_bleed = CreateColor(1, 0, 0.5)
+            color_curse = CreateColor(0.6, 0, 1)
+            color_disease = CreateColor(0.6, 0.4, 0)
+        else
+            color_bleed = CreateColor(0, 0, 0, 0)
+            color_curse = CreateColor(0.6, 0, 1)
+            color_disease = CreateColor(0, 0, 0, 0)
+        end
+    elseif class == "MAGE" then
+        color_curse = CreateColor(0.6, 0, 1)
     elseif class == "MONK" then
         if spec == 2 then
+            color_magic = CreateColor(0.2, 0.6, 1)
             dispellist.Magic = true
         else
+            color_magic = CreateColor(0, 0, 0, 0)
             dispellist.Magic = false
         end
+        color_poison = CreateColor(0, 0.6, 0)
+        color_disease = CreateColor(0.6, 0.4, 0)
     elseif class == "PALADIN" then
         if spec == 1 then
+            color_magic = CreateColor(0.2, 0.6, 1)
             dispellist.Magic = true
         else
+            color_magic = CreateColor(0, 0, 0, 0)
             dispellist.Magic = false
+        end
+        if C_SpellBook.IsSpellKnown(213644) then
+            color_poison = CreateColor(0, 0.6, 0)
+            color_disease = CreateColor(0.6, 0.4, 0)
+        else
+            color_poison = CreateColor(0, 0, 0, 0)
+            color_disease = CreateColor(0, 0, 0, 0)
         end
     elseif class == "PRIEST" then
         if spec == 3 then
+            color_magic = CreateColor(0, 0, 0, 0)
             dispellist.Magic = false
         else
+            color_magic = CreateColor(0.2, 0.6, 1)
             dispellist.Magic = true
         end
+        color_disease = CreateColor(0.6, 0.4, 0)
     elseif class == "SHAMAN" then
         if spec == 3 then
+            color_magic = CreateColor(0.2, 0.6, 1)
             dispellist.Magic = true
         else
+            color_magic = CreateColor(0, 0, 0, 0)
             dispellist.Magic = false
         end
+        if C_SpellBook.IsSpellKnown(383013) then
+            color_poison = CreateColor(0, 0.6, 0)
+        else
+            color_poison = CreateColor(0, 0, 0, 0)
+        end
+        color_curse = CreateColor(0.6, 0, 1)
+    end
+
+    dispelColorCurve:SetToDefaults()
+    dispelColorCurve:ClearPoints()
+
+    local dispelIndex = {
+        [1] = color_magic,
+        [2] = color_curse,
+        [3] = color_disease,
+        [4] = color_poison,
+        [9] = CreateColor(0, 0, 0, 0),
+        [11] = color_bleed,
+    }
+
+    for i, color in pairs(dispelIndex) do
+        dispelColorCurve:AddPoint(i, color)
     end
 end
 
 local function Update(object, _, unit)
     if object.unit ~= unit then return end
-    local debuffType, texture = GetDebuffType(unit, object.DebuffHighlightFilter)
-    if debuffType then
-        local color = DebuffTypeColor[debuffType]
+    if not UnitCanAssist("player", unit) then
         if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder then
             if object.DebuffHighlightBackdrop then
-                object:SetBackdropColor(color.r, color.g, color.b, object.DebuffHighlightAlpha or 1)
+                local c = origColors[object]
+                if c then object:SetBackdropColor(c.r, c.g, c.b, c.a) end
             end
             if object.DebuffHighlightBackdropBorder then
-                object:SetBackdropBorderColor(color.r, color.g, color.b, object.DebuffHighlightAlpha or 1)
+                local c = origBorderColors[object]
+                if c then object:SetBackdropBorderColor(c.r, c.g, c.b, c.a) end
             end
-        elseif object.DebuffHighlightUseTexture then
-            object.DebuffHighlight:SetTexture(texture)
-        else
-            object.DebuffHighlight:SetVertexColor(color.r, color.g, color.b, object.DebuffHighlightAlpha or 0.5)
+        elseif object.DebuffHighlight then
+            local c = origColors[object]
+            if c then object.DebuffHighlight:SetVertexColor(c.r, c.g, c.b, c.a) end
+        end
+        return
+    end
+
+    local filter = object.DebuffHighlightFilter
+    local aura, color
+
+    if filter then
+        aura = C_UnitAuras.GetAuraDataByIndex(unit, 1, "HARMFUL|RAID_PLAYER_DISPELLABLE")
+    else
+        aura = C_UnitAuras.GetAuraDataByIndex(unit, 1, "HARMFUL")
+    end
+
+    if aura and aura.auraInstanceID then
+        color = C_UnitAuras.GetAuraDispelTypeColor(unit, aura.auraInstanceID, dispelColorCurve)
+    end
+
+    if color and color.a and color.a > 0 then
+        local r, g, b, a = color:GetRGBA()
+        local alpha = object.DebuffHighlightAlpha or 0.5
+        if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder then
+            if object.DebuffHighlightBackdrop then
+                object:SetBackdropColor(r, g, b, alpha)
+            end
+            if object.DebuffHighlightBackdropBorder then
+                object:SetBackdropBorderColor(r, g, b, alpha)
+            end
+        elseif object.DebuffHighlight then
+            object.DebuffHighlight:SetVertexColor(r, g, b, alpha)
         end
     else
         if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder then
-            local color
             if object.DebuffHighlightBackdrop then
-                color = origColors[object]
-                object:SetBackdropColor(color.r, color.g, color.b, color.a)
+                local c = origColors[object]
+                if c then object:SetBackdropColor(c.r, c.g, c.b, c.a) end
             end
             if object.DebuffHighlightBackdropBorder then
-                color = origBorderColors[object]
-                object:SetBackdropBorderColor(color.r, color.g, color.b, color.a)
+                local c = origBorderColors[object]
+                if c then object:SetBackdropBorderColor(c.r, c.g, c.b, c.a) end
             end
-        elseif object.DebuffHighlightUseTexture then
-            object.DebuffHighlight:SetTexture(nil)
-        else
-            local color = origColors[object]
-            object.DebuffHighlight:SetVertexColor(color.r, color.g, color.b, color.a)
+        elseif object.DebuffHighlight then
+            local c = origColors[object]
+            if c then object.DebuffHighlight:SetVertexColor(c.r, c.g, c.b, c.a) end
         end
     end
 end
 
 local function Enable(object)
-    -- If we're not highlighting this unit return
     if not object.DebuffHighlightBackdrop and not object.DebuffHighlightBackdropBorder and not object.DebuffHighlight then
         return
     end
-    -- If we're filtering highlights and we're not of the dispelling type, return
+
     if object.DebuffHighlightFilter and not CanDispel[class] then
         return
     end
 
-    -- Make sure aura scanning is active for this object
     object:RegisterEvent("UNIT_AURA", Update)
     object:RegisterEvent("PLAYER_TALENT_UPDATE", CheckSpec, true)
     CheckSpec()
 
     if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder then
         local r, g, b, a = object:GetBackdropColor()
-        origColors[object] = {r = r, g = g, b = b, a = a}
+        origColors[object] = { r = r, g = g, b = b, a = a }
         r, g, b, a = object:GetBackdropBorderColor()
-        origBorderColors[object] = {r = r, g = g, b = b, a = a}
-    elseif not object.DebuffHighlightUseTexture then
+        origBorderColors[object] = { r = r, g = g, b = b, a = a }
+    elseif object.DebuffHighlight then
         local r, g, b, a = object.DebuffHighlight:GetVertexColor()
-        origColors[object] = {r = r, g = g, b = b, a = a}
+        origColors[object] = { r = r, g = g, b = b, a = a }
     end
 
     return true
