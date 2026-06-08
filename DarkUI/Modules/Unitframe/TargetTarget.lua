0aacd17b13d8afd7f9535dcc8f896c4dd6a4d21d
SolardiaX <solardiax@hotmail.com>
2026-06-08 23:54:16 +0800

    fix: [Unitframe] add Health.bg to FocusTarget and TargetTarget frames

--- DarkUI/Modules/Unitframe/TargetTarget.lua
+++ DarkUI/Modules/Unitframe/TargetTarget.lua
@@ -57,6 +57,11 @@ local function createBar(self)
     self.Health:SetStatusBarTexture(media.hpTex)
     self.Health:SetStatusBarColor(0.2, 0.2, 0.2)
 
+    self.Health.bg = self.Health:CreateTexture(nil, 'BORDER')
+    self.Health.bg:SetTexture(media.hpTex)
+    self.Health.bg:SetAllPoints(self.Health)
+    self.Health.bg.multiplier = 0.3
+
     self.Health.frequentUpdates = true
     self.Health.colorSmooth = true
     self.Health.smoothing = Enum.StatusBarInterpolation.Continuous
